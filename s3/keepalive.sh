0584bef4959f47febdb5b722426ebe74a7028ae7
Scalyr Inc <support@scalyr.com>
2015-12-08 23:54:08 +0000

    Checking in pending changes on the control server

--- s3/keepalive.sh
+++ s3/keepalive.sh
@@ -7,6 +7,6 @@ fi
 
 ps -ef | grep "[j]ava.*s3bench" &> /dev/null
 if [ $? -ne 0 ]; then
-    $BASE_DIR/run.sh $BASE_DIR/s3bench-1.0.3-jar-with-dependencies.jar $BASE_DIR/log4j2.xml &> $BASE_DIR/output-`date "+%Y-%m-%dT%H:%M"`.log&
+    $BASE_DIR/run.sh $BASE_DIR/s3bench-1.0.5-jar-with-dependencies.jar $BASE_DIR/log4j2.xml &> $BASE_DIR/output-`date "+%Y-%m-%dT%H:%M"`.log&
 fi
 
