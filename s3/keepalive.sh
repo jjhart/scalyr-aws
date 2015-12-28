a8f4fc2c96a7c23c573b6fcd1f0b323d06c5aa6b
John Hart <jhart@insidesales.com>
2015-12-28 12:22:41 -0800

    Update s3bench version to 1.0.8; remove hardcoded jarfile name from keepalive.sh

--- s3/keepalive.sh
+++ s3/keepalive.sh
@@ -5,8 +5,12 @@ if [ -z "$1" ]; then
    BASE_DIR=$HOME 
 fi
 
+# use "latest" (alphasort) jarfile in case of > 1.  Should only be one, as "provision.sh" copies only one to local machine
+JARFILE=$(ls -1 $BASE_DIR/s3bench-1.0.?-jar-with-dependencies.jar | tail -1)
+
+
 ps -ef | grep "[j]ava.*s3bench" &> /dev/null
 if [ $? -ne 0 ]; then
-    $BASE_DIR/run.sh $BASE_DIR/s3bench-1.0.6-jar-with-dependencies.jar $BASE_DIR/instance.properties &> $BASE_DIR/output-`date "+%Y-%m-%dT%H:%M"`.log&
+    $BASE_DIR/run.sh $JARFILE $BASE_DIR/instance.properties &> $BASE_DIR/output-`date "+%Y-%m-%dT%H:%M"`.log&
 fi
 
